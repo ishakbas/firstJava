666aad0748e062316c2b093dbdd293828a26a6a9
ishakbas <brawlcygan@gmail.com>
2022-08-09 16:27:45 +0300

    solution2

--- src/Solution2.java
+++ src/Solution2.java
@@ -1,5 +1,13 @@
 public class Solution2 {
     public static void main(String[] args) {
+        System.out.println(minNumber(-2,2,3,4));
+    }
+    private static int minNumber(int a,int b,int c,int d) {
+        int min = a;
+        if (b<min) min = b;
+        if (c<min) min = c;
+        if (d<min) min = d;
+        return min;
 
     }
 }
