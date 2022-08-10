e7ed067c37ceb2522b59487f95b841135e9dde38
ishakbas <brawlcygan@gmail.com>
2022-08-10 14:58:11 +0300

    solution10

---
+++ src/Soulution10.java
@@ -0,0 +1,10 @@
+public class Soulution10 {
+    public static void main(String[] args) {
+        marryMe("m","m");
+        marryMe("j","d");
+    }
+    private static void marryMe(String gend1, String gend2) {
+        if (gend1.equals(gend2)) System.out.println("Go to USA");
+        else System.out.println("Marry");
+    }
+}
