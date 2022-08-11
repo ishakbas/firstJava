6f13098eeccff6c593e41411a515309557e70132
ishakbas <brawlcygan@gmail.com>
2022-08-11 16:16:30 +0300

    Solution1_array

--- src/Solution1_array.java
+++ src/Solution1_array.java
@@ -1,12 +1,12 @@
 public class Solution1_array {
     public static void main(String[] args) {
-        System.out.println(returnMin(new int[]{265,324,444,52,123,345}));
+        System.out.println(returnMin(new int[]{265,324,44,52,123,345}));
     }
     private static int returnMin(int[] arrayOfNum) {
         int min = arrayOfNum[0];
-        for (int j : arrayOfNum) {
-            if (min > j) {
-                min = j;
+        for (int i = 0; i < arrayOfNum.length; i++) {
+            if (min > arrayOfNum[i]) {
+                min = arrayOfNum[i];
             }
         }
         return min;
